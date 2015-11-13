e3c244bf8da67bbbaeda5d746f6f080e56eb5caf
sgerbeth <stephan.gerbeth@t8y.com>
2015-11-13 16:31:59 +0100

    update index.js reduce tasks

--- index.js
+++ index.js
@@ -1,54 +1,32 @@
 "use strict";
+
+var template = require('lodash/string/template');
 var options = require('minimist')(process.argv.slice(2));
+var serverConfig = require((process.cwd() + '/' + options.serverConfig || (process.cwd() + '/' + './env/config/local.json')))[(options.env || 'development')];
+var tasksConfig = JSON.parse(template(JSON.stringify(require(process.cwd() + '/' + options.configTasks)))({'destination': serverConfig.dest}));
 
-module.exports = function(server, config) {
-    var assemble = require('./lib/assemble/config');
-    var runSequence = require('run-sequence').use(assemble);
-    var livereload = require('gulp-livereload');
+var assemble = require('./lib/assemble/config');
+var runSequence = require('run-sequence').use(assemble);
+var livereload = require('gulp-livereload');
 
-    assemble.task('clean', require('./lib/tasks/clean')('clean', config.tasks.clean));
-    assemble.task('copy', require('./lib/tasks/copy')('copy', config.tasks.copy));
-    assemble.task('handlebars', require('./lib/tasks/handlebars')('handlebars', config.tasks.handlebars, server));
-    assemble.task('postcss', require('./lib/tasks/postcss')('postcss', config.tasks.postcss));
-    assemble.task('purecss', require('./lib/tasks/purecss')(config.tasks.purecss));
-    assemble.task('sitemap', require('./lib/tasks/sitemap')('sitemap', config.tasks.sitemap));
-    assemble.task('webpack', require('./lib/tasks/webpack')('webpack', config.tasks.webpack)());
-    assemble.task('watch', function(cb) {
-        if(server.livereload) {
-            livereload.listen({port: server.livereload.port});
-        }
-        cb();
-    });
+assemble.task('clean', require('./lib/tasks/clean')('clean', tasksConfig.clean));
+assemble.task('copy', require('./lib/tasks/copy')('copy', tasksConfig.copy));
+assemble.task('handlebars', require('./lib/tasks/handlebars')('handlebars', tasksConfig.handlebars, serverConfig));
+assemble.task('postcss', require('./lib/tasks/postcss')('postcss', tasksConfig.postcss));
+assemble.task('purecss', require('./lib/tasks/purecss')(tasksConfig.purecss));
+assemble.task('sitemap', require('./lib/tasks/sitemap')('sitemap', tasksConfig.sitemap));
+assemble.task('webpack', require('./lib/tasks/webpack')('webpack', tasksConfig.webpack)());
+assemble.task('watch', function(cb) {
+    if(serverConfig.livereload) {
+        livereload.listen({port: serverConfig.livereload.port});
+    }
+    cb();
+});
 
-    assemble.task('default', ['watch', 'server']);
+assemble.task('build', function(callback) {
+    runSequence('prebuild', 'webpack:app', callback);
+});
 
-    assemble.task('run', function(callback) {
-        if(options.env === 'development') {
-            runSequence('prebuild', 'default', callback);
-        } else {
-            runSequence('build', 'server', callback);
-        }
-    });
-
-    assemble.task('build', function(callback) {
-        runSequence('prebuild', 'webpack:app', callback);
-    });
-
-    assemble.task('prebuild', function(callback) {
-        runSequence('clean', ['copy', 'webpack:embed', 'purecss'], 'postcss', 'handlebars', ['sitemap'], callback);
-    });
-
-    assemble.task('server', function () {
-        if(options.env === 'development') {
-            require('gulp-nodemon')({
-                script: require.resolve(config.server.name),
-                ignore: ['src/**/*'],
-                args: ['--config=' + options.config]
-            });
-        } else {
-            require(config.server.name);
-        }
-    });
-};
-
-process.once('SIGINT', function() { process.exit(0); });
+assemble.task('prebuild', function(callback) {
+    runSequence('clean', ['copy', 'webpack:embed', 'purecss'], 'postcss', 'handlebars', ['sitemap'], callback);
+});
