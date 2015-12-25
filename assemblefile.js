0a63027b1bf4bfaa167f4265b1f978a3fcfac477
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2015-12-25 19:39:08 +0100

    updated assemble@0.6.2

--- assemblefile.js
+++ assemblefile.js
@@ -5,18 +5,18 @@ var options = require('minimist')(process.argv.slice(2));
 var serverConfig = JSON.parse(template(JSON.stringify(require((process.cwd() + '/' + options.serverConfig || (process.cwd() + '/' + './env/config/local.json')))))({'root': process.cwd()}))[(options.env || 'development')];
 var tasksConfig = JSON.parse(template(JSON.stringify(require(process.cwd() + '/' + options.configTasks)))({'destination': serverConfig.dest, 'root': process.cwd()}));
 
-var assemble = require('./lib/assemble/config');
-var runSequence = require('run-sequence').use(assemble);
+var gulp = require('gulp');
+var runSequence = require('run-sequence').use(gulp);
 var livereload = require('gulp-livereload');
 
-assemble.task('clean', require('./lib/tasks/clean')('clean', tasksConfig.clean, serverConfig));
-assemble.task('copy', require('./lib/tasks/copy')('copy', tasksConfig.copy, serverConfig));
-assemble.task('handlebars', require('./lib/tasks/handlebars')('handlebars', tasksConfig.handlebars, serverConfig));
-assemble.task('postcss', require('./lib/tasks/postcss')('postcss', tasksConfig.postcss, serverConfig));
-assemble.task('purecss', require('./lib/tasks/purecss')(tasksConfig.purecss));
-assemble.task('sitemap', require('./lib/tasks/sitemap')('sitemap', tasksConfig.sitemap, serverConfig));
-assemble.task('webpack', require('./lib/tasks/webpack')('webpack', tasksConfig.webpack, serverConfig)());
-assemble.task('watch', function(cb) {
+gulp.task('clean', require('./lib/tasks/clean')('clean', tasksConfig.clean, serverConfig));
+gulp.task('copy', require('./lib/tasks/copy')('copy', tasksConfig.copy, serverConfig));
+gulp.task('handlebars', require('./lib/tasks/handlebars')('handlebars', tasksConfig.handlebars, serverConfig));
+gulp.task('postcss', require('./lib/tasks/postcss')('postcss', tasksConfig.postcss, serverConfig));
+gulp.task('purecss', require('./lib/tasks/purecss')(tasksConfig.purecss));
+gulp.task('sitemap', require('./lib/tasks/sitemap')('sitemap', tasksConfig.sitemap, serverConfig));
+gulp.task('webpack', require('./lib/tasks/webpack')('webpack', tasksConfig.webpack, serverConfig)());
+gulp.task('watch', function(cb) {
     if(serverConfig.livereload) {
         livereload.listen({
             port: serverConfig.livereload.port
@@ -25,10 +25,10 @@ assemble.task('watch', function(cb) {
     cb();
 });
 
-assemble.task('build', function(callback) {
+gulp.task('build', function(callback) {
     runSequence('prebuild', 'webpack:app', callback);
 });
 
-assemble.task('prebuild', function(callback) {
+gulp.task('prebuild', function(callback) {
     runSequence('clean', ['copy', 'webpack:embed', 'purecss'], 'postcss', 'handlebars', ['sitemap'], callback);
 });
