83a3eeae9d5a7291f62e7a46e8c33df0a163a2c0
Thorn Walli <lammpee@gmail.com>
2016-10-24 21:13:16 +0200

    last master updates

--- assemblefile.js
+++ assemblefile.js
@@ -4,11 +4,12 @@ require('colors');
 var assemble = require('./lib/assemble/config');
 var template = require('lodash/template');
 var options = require('minimist')(process.argv.slice(2));
+var upath = require('upath');
 
-var serverConfig = JSON.parse(template(JSON.stringify(require((process.cwd() + '/' + options.serverConfig || (process.cwd() + '/' + './env/config/local.json')))))({
+var serverConfig = JSON.parse(template(JSON.stringify(require(upath.join(process.cwd(), options.serverConfig) || upath.join(process.cwd(), './env/config/local.json'))))({
     'root': process.cwd()
 }))[(options.env || 'development')];
-var tasksConfig = JSON.parse(template(JSON.stringify(require(process.cwd() + '/' + options.configTasks)))({
+var tasksConfig = JSON.parse(template(JSON.stringify(require(upath.join(process.cwd(), options.configTasks))))({
     'destination': serverConfig.dest,
     'root': process.cwd()
 }));
@@ -20,7 +21,7 @@ var livereload = require('gulp-livereload');
 
 // handlebars helpers
 
-(tasksConfig.handlebars.helpers || []).forEach(function(helper) {
+(tasksConfig.handlebars.helpers || []).forEach(function (helper) {
     assemble.asyncHelper(helper.name, require(helper.src).apply(require, {
         assemble: assemble,
         config: helper.config
@@ -40,7 +41,7 @@ gulp.task('postcss', require('./lib/tasks/postcss')('postcss', tasksConfig.postc
 gulp.task('purecss', require('./lib/tasks/purecss')(tasksConfig.purecss));
 gulp.task('sitemap', require('./lib/tasks/sitemap')('sitemap', tasksConfig.sitemap, serverConfig));
 gulp.task('webpack', require('./lib/tasks/webpack')('webpack', tasksConfig.webpack, serverConfig)());
-gulp.task('watch', function(cb) {
+gulp.task('watch', function (cb) {
     if (serverConfig.livereload) {
         livereload.listen({
             port: serverConfig.livereload.port
@@ -52,21 +53,21 @@ gulp.task('watch', function(cb) {
 // build
 
 gulp.task('build', function(callback) {
-    runSequence('prebuild', 'webpack:app', callback);
+    runSequence('prebuild', 'webpack:app', 'zip-compress:default', callback);
 });
 
-gulp.task('prebuild', function(callback) {
+gulp.task('prebuild', function (callback) {
     runSequence('register-packages:default', 'clean', ['copy', 'fontmin', 'webpack:embed', 'purecss'], 'postcss', 'handlebars', ['sitemap'], callback);
 });
 
 // banner build
 
-gulp.task('build-banner', function(callback) {
-    runSequence('clean', ['copy', 'fontmin', 'webpack:app', 'postcss'], 'handlebars', 'zip-compress:banner', callback);
+gulp.task('build-banner', function (callback) {
+    runSequence('clean', ['copy', 'fontmin', 'webpack:embed', 'webpack:app', 'postcss'], 'handlebars', 'zip-compress:banner', callback);
 });
 
-gulp.task('prebuild-banner', function(callback) {
-    runSequence('clean', ['copy', 'fontmin', 'postcss'], 'handlebars', callback);
+gulp.task('prebuild-banner', function (callback) {
+    runSequence('clean', ['copy', 'fontmin', 'webpack:embed', 'postcss'], 'handlebars', callback);
 });
 
 // register-packages
@@ -93,7 +94,7 @@ if (tasksConfig.exporthbs) {
     console.log('[' + 'task'.gray + '][' + 'export-hbs'.gray + ']', 'Missing Config'.bold.red);
 }
 if (tasksConfig.exporthbs) {
-    gulp.task('export', function(callback) {
+    gulp.task('export', function (callback) {
         runSequence('clean', 'register-packages:default', 'export-hbs', callback);
     });
 }
